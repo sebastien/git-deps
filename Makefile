6597d8183195a095dab4c5a7a541f0e562469a8b
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-09-23 10:10:14 +1200

    [Update] basic deps

---
+++ Makefile
@@ -0,0 +1,34 @@
+SOURCES_BASH=$(wildcard *.sh bin/*sh src/sh/*sh tests/*.sh research/*.sh)
+USER?=$(shell whoami)
+HOME?=/home/$(USER)
+
+test:
+	@bash tests/harness.sh
+
+lint:
+	@shellcheck $(SOURCES_BASH)
+
+fmt:
+	@shfmt -w $(SOURCES_BASH)
+
+install:
+	@mkdir -p "$(HOME)/.local/bin"
+	TARGET="$(HOME)/.local/bin/git-deps"
+	if [ -e "$$TARGET" ]; then
+		curl -o "$$TARGET" 'https://raw.githubusercontent.com/sebastien/git-deps/master/bin/git-deps'
+		chmod +x "$$TARGET"
+	fi
+
+install-link:
+	@mkdir -p "$(HOME)/.local/bin"
+	TARGET="$(HOME)/.local/bin/git-deps"
+	if [ -e "$$TARGET" ]; then
+		unlink "$$TARGET"
+	fi
+	ln -sfr bin/git-kv "$$TARGET"
+
+print-%:
+	@$(info $*=$($*))
+
+.ONESHELL:
+# EOF
--- src/sh/git-deps.sh
+++ src/sh/git-deps.sh
@@ -3,17 +3,30 @@
 GIT_DEPS_FILE=".gitdeps"
 GIT_DEPS_SOURCE="file"
 
-function git_deps_log_message {
-	echo "$@" > /dev/stdout
+function git_deps_log_action {
+	echo "--> $@" >/dev/stdout
 	return 0
 }
 
+function git_deps_log_message {
+	echo "$@" >/dev/stdout
+	return 0
+}
+
+function git_log_output {
+	echo "$1"
+	shift
+	while IFS= read -r line; do
+		echo " » $line"
+	done <<< "$*"
+}
+
 function git_deps_log_error {
-	echo "!!! ERR $@" > /dev/stderr
+	echo "!!! ERR $*" >/dev/stderr
 	return 1
 }
 
-function git_deps_read_file  {
+function git_deps_read_file {
 	if [ -e "$GIT_DEPS_FILE" ]; then
 		cat "$GIT_DEPS_FILE"
 		return 0
@@ -24,103 +37,120 @@ function git_deps_read_file  {
 
 function git_deps_read {
 	case "$GIT_DEPS_SOURCE" in
-		file)
-			git_deps_read_file "$@"
-			return 0
-			;;
-		*)
-			git_deps_log_error "Unsupported source: $GIT_DEPS_SOURCE"
-			return 1
-			;;
+	file)
+		git_deps_read_file 
+		return 0
+		;;
+	*)
+		git_deps_log_error "Unsupported source: $GIT_DEPS_SOURCE"
+		return 1
+		;;
 	esac
 
 }
 
 # function git_deps_save {
 # }
-# 
+#
 # function git_deps_status {
 # }
-# 
+#
 # function git_deps_pull {
-# 
+#
 # }
-# 
+#
 # function git_deps_push {
-# 
+#
 # }
 
-function git_deps_check {
+function git_deps_status {
 	local path="$1"
 	local rev="$2"
-	local modified;modified="$(git -C "$path" status --porcelain | grep -v '??')"
+	local modified
+	modified="$(git -C "$path" status --porcelain | grep -v '??')"
 	# ` M` for modified
 	# `??` for added but untracked
 	if [ -n "$modified" ]; then
-		echo "Outstanding modifications in: $path"
-		echo "$modified"
-		return 1
+		echo -n "no-modified"
 	else
-		local expected;expected=$(git -C "$path" rev-parse "$rev" 2> /dev/null ||echo -n "expected-not-found")
-		local current;current=$(git -C "$path" rev-parse HEAD 2> /dev/null || echo -n "current-not-found")
-		if [ "$expected"  != "$current" ]; then
+		local expected
+		expected=$(git -C "$path" rev-parse "$rev" 2>/dev/null || echo -n "expected-not-found")
+		local current
+		current=$(git -C "$path" rev-parse HEAD 2>/dev/null || echo -n "current-not-found")
+		if [ "$expected" != "$current" ]; then
 			# Is expected an ancestor of current (current is ahead)
 			if git -C "$path" merge-base --is-ancestor "$expected" "$current"; then
 				# TODO: We should have a force argument to proceed there
-				git_deps_log_message "Current branch is ahead of expected"
-				return 1
+				echo -n "maybe-ahead"
 			# Is current an ancestor of expected (current is behind)
 			elif git -C "$path" merge-base --is-ancestor "$current" "$expected"; then
-				git_deps_log_message "Current branch is behind expected"
-				return 0
+				echo -n "ok-behind"
 			elif [ -z "$(git -C "$path" branch -r --contains "$current")" ]; then
-				git_deps_log_message "Current branch contains local modifications not in remote, aborting"
-				return 1
+				echo -n "no-unsynced"
 			else
-				git_deps_log_message "Current branch is different from remote"
-				return 0
+				echo -n "ok-synced"
 			fi
+		else
+			echo -n "ok-same"
 		fi
 	fi
 }
 
+function git_deps_update {
+	local path="$1"
+	local rev="$2"
+	git_log_output "up $path → $rev" "$(git -C "$path" checkout "$rev" 2>&1)"
+}
 
 function git_deps_ensure {
 	local path="$1"
 	local repo="$2"
 	local rev="${3:-main}"
 	if [ -z "$path" ]; then
-		git_deps_log_error "Dependency missing directory: $@"
+		git_deps_log_error "Dependency missing directory: $*"
 		return 1
 	elif [ -z "$repo" ]; then
-		git_deps_log_error "Dependency missing repository: $@"
+		git_deps_log_error "Dependency missing repository: $*"
 		return 1
 	elif [ -z "$rev" ]; then
-		git_deps_log_error "Dependency missing revision: $@"
+		git_deps_log_error "Dependency missing revision: $*"
 		return 1
 	fi
 	if [ ! -e "$path" ]; then
+		git_deps_log_action "Retrieving dependency: $path ← $repo [$rev]"
 		git clone "$repo" "$path"
 	fi
-	if git_deps_check "$path" "$rev"; then
-		echo "OK"
+	set -a STATUS
+	IFS='-' read -ra STATUS <<<"$(git_deps_status "$path" "$rev")"
+	case "${STATUS[0]}" in
+		ok)
+			case "${STATUS[1]}" in 
+				behind)
+					git_deps_update "$path" "$rev"
+					;;
+				*)
+					;;
+			esac
+			;;
+		maybe)
+			;;
+		*)
+			;;
+	esac
+	echo "${STATUS[0]} [${STATUS[1]}] $path@$rev"
 
-	else
-		echo "NOT OK"
-		return 1
-	fi
 }
 
 # --
 # Updates the deps file.
-function git_deps_update {
+function git-deps-update {
 	IFS=$'\n'
 	for LINE in $(git_deps_read); do
 		set -a FIELDS
-		IFS='|' read -ra FIELDS <<< "$LINE"
+		IFS='|' read -ra FIELDS <<<"$LINE"
 		git_deps_ensure "${FIELDS[@]}"
 	done
 }
 
-git_deps_update
+git-deps-update
 # EOF
