6e28786c1422dc3d122f412307f4c61102ad27d2
Sébastien Pierre <sebastien.pierre@gmail.com>
2024-11-01 09:57:09 +1300

    [New] jj: support for jj

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# git-deps
-Git submodule alternative
+```
+  _____ _ _      ____
+ |   __|_| |_   |    \ ___ ___ ___
+ |  |  | |  _|  |  |  | -_| . |_ -|
+ |_____|_|_|    |____/|___|  _|___|
+                          |_|
+```
+
+
+`git-deps` (and `jj-deps`) is a Git submodule alternative with the following
+features:
+
+- Works with both `git` and `jj`
+- Ensures that your clones always succeed, even if your dependencies are
+  not available anymore.
+
+
+# Quick start
+
+In your repository create a `.gitdeps` file keeps track of your dependencies:
+
+```
+deps/appenv|git@github.com:sebastien/appenv.git|master
+deps/git-kv|git@github.com:sebastien/git-kv.git|main
+```
+
+`git-deps status` will tell you the status of the dependencies, wether they're
+checked out or not
+
+```
+$ git-deps status
+deps/appenv master ok-same
+deps/git-kv main ok-same new commits…
+```
+
+To bring your dependencies up to data, do `git-deps pull`, this will likely
+succeed, unless you have local changes or have unsynced changes.
+
+```
+deps/appenv|git@github.com:sebastien/appenv.git|master
+ok-same
+ → [deps/appenv] Pulling master…
+From github.com:sebastien/appenv
+ * branch            master     -> FETCH_HEAD
+Already up to date.
+```
+
+You can try a `git-deps push` to push your changes (in case you have no
+local modifications), or alternatively `cd` into your dependency directory
+and resolve the problem, typically using a `commit` of your local modifications
+and a `merge` or `push`, so that a pull is successful.
+
+Whenever you want to save the current state of your dependencies, do  `git-deps save`.
+
+
--- src/sh/git-deps.sh
+++ src/sh/git-deps.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
+#  _____ _ _      ____              
+# |   __|_| |_   |    \ ___ ___ ___ 
+# |  |  | |  _|  |  |  | -_| . |_ -|
+# |_____|_|_|    |____/|___|  _|___|
+#                          |_|      
+
+GIT_DEPS_MODE=git
 GIT_DEPS_FILE=".gitdeps"
 GIT_DEPS_SOURCE="file"
-
-function git_deps_log_mark {
-	# Save cursor position
-	echo -en '\033[s'
-}
-
-function git_deps_log_clear {
-	# Clear
-	echo -en '\033[1G\033[K'
-	echo -en '\033[u'
-}
+if [ -d ".jj" ]; then
+	GIT_DEPS_MODE="jj"
+fi
+case "$0" in 
+	*jj-deps)
+		GIT_DEPS_MODE=jj
+		;;
+esac
 
 function git_deps_log_action {
-	echo "$@" >/dev/stdout
+	echo " → $@" >&2
 	return 0
 }
 
 function git_deps_log_message {
-	echo "$@" >/dev/stdout
+	echo " … $@" >&2
 	return 0
 }
 
 function git_deps_log_tip {
-	echo "$@" >/dev/stdout
+	echo " ✱ $@" >&2
 	return 0
 }
 
@@ -38,7 +42,7 @@ function git_log_output {
 }
 
 function git_deps_log_error {
-	echo "!!! ERR $*" >/dev/stderr
+	echo "!!! ERR $*" &1>2
 	return 1
 }
 
@@ -63,6 +67,10 @@ function git_deps_read_file {
 	fi
 }
 
+function git_deps_write_file {
+	echo "$@" | sed 's/|/[[:space:]]\+/g'  > "$GIT_DEPS_FILE"
+}
+
 function git_deps_read {
 	case "$GIT_DEPS_SOURCE" in
 	file)
@@ -74,8 +82,123 @@ function git_deps_read {
 		return 1
 		;;
 	esac
+}
+
+function git_deps_write {
+	case "$GIT_DEPS_SOURCE" in
+	file)
+		git_deps_write_file "$@" 
+		return 0
+		;;
+	*)
+		git_deps_log_error "Unsupported source: $GIT_DEPS_SOURCE"
+		return 1
+		;;
+	esac
+}
+
+
+
+# ----------------------------------------------------------------------------
+# 
+# GIT/JJ WRAPPER
+#
+# ----------------------------------------------------------------------------
+
+function git_deps_op_clone {
+	local repo="$1"
+	local path="$2"
+	local parent="$(dirname "$2")"
+	if [ ! -e "$parent" ]; then
+		mkdir -p "$parent"
+	fi
+	if [ "$GIT_DEPS_MODE" == "jj" ]; then
+		jj git clone --colocate "$repo" "$path"
+	else
+		git clone "$repo" "$path"
+	fi
+}
+
+
+function git_deps_op_fetch {
+	local path="$1"
+	git -C "$path" fetch
+}
+
+function git_deps_op_checkout {
+	local path="$1"
+	local rev="$2"
+	git -C "$path" checkout "$rev"
 
 }
+
+
+# --
+# Returns a non-empty string if there are local changes.
+function git_deps_op_localchanges {
+	if [ "$GIT_DEPS_MODE" == "jj" ]; then
+		# Working copy changes:
+		# A .gitdeps
+		# Working copy : klywuowv ee6d952f (no description set)
+		# Parent commit: zzzzzzzz 00000000 (empty) (no description set)
+		jj -R "$path" status | head -n -2 | tail -n +2
+	else
+		git -C "$path" status --porcelain | grep -v '??'
+	fi
+}
+
+# --
+# Returns the (git) commit id for the current revision
+function git_deps_op_commit_id {
+	if ! git -C "$1" rev-parse "${2:-HEAD}" 2>/dev/null; then
+		return 1
+	fi
+}
+
+# --
+# Tells if the current revision is a named branch `branch`, or
+# an unnamed commit `hash`, or if it is simply unknown.
+function git_deps_op_identify_rev {
+	if git show-ref --quiet --heads "$1" || git show-ref --quiet --tags "$1"; then
+		echo "branch"
+	elif git rev-parse --verify "$1^{commit}" >/dev/null 2>&1; then
+		echo "hash"
+	else
+		echo "unknown"
+	fi
+}
+
+# --
+# Takes `PATH` `EXPECTED` `CURRENT` and returns one of the following:
+# - `ok-same` both repvisions are the same
+# - `ok-behind` current is behind expected (can fast-forward)
+# - `ok-synced` 
+# - `maybe-ahead` current may be ahead of behind (may need a merge)
+# - `no-unsynced` current version is is not 
+function git_deps_op_status {
+	local path="$1"
+	local expected="$2"
+	local current="$2"
+	if [ "$expected" == "$current" ]; then
+		echo "ok-same"
+	elif git -C "$path" merge-base --is-ancestor "$expected" "$current"; then
+		# TODO: We should have a force argument to proceed there
+		echo  "maybe-ahead"
+	# Is current an ancestor of expected (current is behind)
+	elif git -C "$path" merge-base --is-ancestor "$current" "$expected"; then
+		echo "ok-behind"
+	elif [ -z "$(git -C "$path" branch -r --contains "$current")" ]; then
+		echo "no-unsynced"
+	else
+		echo "ok-synced"
+	fi
+}
+
+# ----------------------------------------------------------------------------
+# 
+# HIGH LEVEL COMMANDS
+#
+# ----------------------------------------------------------------------------
 
 # function git_deps_save {
 # }
@@ -88,17 +211,6 @@ function git_deps_read {
 #
 # }
 
-
-function git_identify_rev {
-	if git show-ref --quiet --heads "$1" || git show-ref --quiet --tags "$1"; then
-		echo "branch"
-	elif git rev-parse --verify "$1^{commit}" >/dev/null 2>&1; then
-		echo "hash"
-	else
-		echo "unknown"
-	fi
-}
-
 function git_deps_status {
 	local path="$1"
 	local rev="$2"
@@ -107,50 +219,34 @@ function git_deps_status {
 	if [ ! -e "$path" ] || [ ! -e "$path/.git" ]; then
 		echo "missing"
 	else
-		modified="$(git -C "$path" status --porcelain | grep -v '??')"
+		modified="$(git_deps_op_localchanges)"
 		# ` M` for modified
 		# `??` for added but untracked
 		if [ -n "$modified" ]; then
 			echo "no-modified"
 		else
-			case "$(git_identify_rev "$rev")" in
+			case "$(git_deps_op_identify_rev "$rev")" in
 				branch)
 					# TODO: We should probably not fetch all the time
-					git_deps_log_mark
 					git_deps_log_action "[Fetching new commits…]"
-					git -C "$path" fetch
-					git_deps_log_clear
+					git_deps_op_fetch "$path"
 					rev="origin/$rev"
 					;;
 				hash)
 					;;
 			esac
 			local expected
-			expected=$(git -C "$path" rev-parse "$rev" 2>/dev/null || echo "err-expected_not_found")
+			expected=$(git_deps_op_commit_id "$path"  "$rev" || echo "err-expected_not_found")
 			local current
-			current=$(git -C "$path" rev-parse HEAD 2>/dev/null || echo "err-current_not_found")
-			if [ "$expected" != "$current" ]; then
-				# Is expected an ancestor of current (current is ahead)
-				if git -C "$path" merge-base --is-ancestor "$expected" "$current"; then
-					# TODO: We should have a force argument to proceed there
-					echo  "maybe-ahead"
-				# Is current an ancestor of expected (current is behind)
-				elif git -C "$path" merge-base --is-ancestor "$current" "$expected"; then
-					echo "ok-behind"
-				elif [ -z "$(git -C "$path" branch -r --contains "$current")" ]; then
-					echo "no-unsynced"
-				else
-					echo "ok-synced"
-				fi
-			else
-				echo "ok-same"
-			fi
+			current=$(git_deps_op_commit_id "$path" || echo "err-current_not_found")
+			git_deps_op_status "$path" "$expected" "$current"
 		fi
 	fi
 }
 
 # --
-# Ensures that the given PATH is checked out at the REPO revision.
+# Ensures that the given `PATH` is checked out using `REPO` and the given
+# `REVISION`
 function git_deps_update {
 	local path="$1"
 	local repo="$2"
@@ -167,7 +263,7 @@ function git_deps_update {
 	fi
 	if [ ! -e "$path" ]; then
 		git_deps_log_action "Retrieving dependency: $path ← $repo [$rev]"
-		git clone "$repo" "$path"
+		git_deps_op_clone "$repo" "$path"
 	fi
 	set -a STATUS
 	IFS='-' read -ra STATUS <<<"$(git_deps_status "$path" "$rev")"
@@ -204,9 +300,26 @@ function git-deps-status {
 		set -a FIELDS
 		IFS='|' read -ra FIELDS <<<"$LINE"
 		STATUS=$(git_deps_status "${FIELDS[0]}" "${FIELDS[2]}")
-		echo "${FIELDS[0]} ${FIELDS[2]} ${STATUS} "
+		echo "${FIELDS[0]} ${FIELDS[2]} $(git_deps_op_commit_id "${FIELDS[0]}") → ${STATUS} "
 	done
 }
+
+function git-deps-state {
+	IFS=$'\n'
+	local STATUS
+	for LINE in $(git_deps_read); do
+		set -a FIELDS
+		IFS='|' read -ra FIELDS <<<"$LINE"
+		echo "${FIELDS[0]} ${FIELDS[1]} ${FIELDS[2]} ${FIELDS[3]} $(git_deps_op_commit_id "${FIELDS[0]}")"
+	done
+}
+
+function git-deps-save {
+	local state="$(git-deps-state "$@")"
+	git_deps_write "$state"
+}
+
+
 
 function git-deps-update {
 	IFS=$'\n'
@@ -240,19 +353,19 @@ function git-deps-pull {
 			ok-*|maybe-ahead)
 				git_deps_log_action "[$REPO] Pulling $REV…"
 				if ! git -C "$REPO" pull origin "$REV"; then
-					git_deps_log_error "$REPO: Pull failed"
-					git_deps_log_tip "$REPO: Manual intervention is required to fix"
+					git_deps_log_error "[$REPO] Pull failed"
+					git_deps_log_tip "[$REPO] Maybe revision or branch $REV does not exist in origin repository?"
 					((ERRORS++))
 				fi
 				;;
 			no-*)
-				git_deps_log_error "[$REPO] Cannot merge"
+				git_deps_log_error "[$REPO] Cannot merge $STATUS"
 				((ERRORS++))
 				# TODO: Not sure why/what we can do from there
 				# TODO: Increment errors
 				;;
 			err-*)
-				git_deps_log_error "$REPO: Could not process due to error $STATUS"
+				git_deps_log_error "[$REPO] Could not process due to error $STATUS"
 				((ERRORS++))
 				;;
 			missing)
@@ -260,13 +373,13 @@ function git-deps-pull {
 				if [ ! -e "$(dirname "$REPO")" ]; then
 					mkdir -p $(dirname "$REPO")
 				fi
-				if ! git clone "$URL" "$REPO"; then
-					git_deps_log_error "$REPO: Clone failed"
-					git_deps_log_tip "$REPO: Manual intervention is required to fix"
+				if ! git_deps_op_clone "$URL" "$REPO"; then
+					git_deps_log_error "[$REPO] Clone failed"
+					git_deps_log_tip "[$REPO] Manual intervention is required to fix"
 					((ERRORS++))
-				elif ! git -C "$REPO" checkout "$REV"; then
-					git_deps_log_error "$REPO: Checkout failed"
-					git_deps_log_tip "$REPO: Manual intervention is required to fix"
+				elif ! git_deps_op_checkout "$REPO" "$REV"; then
+					git_deps_log_error "[$REPO] Checkout failed"
+					git_deps_log_tip "[$REPO] Maybe revision or branch $REV does not exist in origin repository?"
 					((ERRORS++))
 				fi
 				;;
@@ -299,6 +412,14 @@ function git-deps {
 			fi
 
 			;;
+		state|s)
+			shift
+			git-deps-state "$@"
+			;;
+		save|s)
+			shift
+			git-deps-save "$@"
+			;;
 		sync|sy)
 			shift
 			if git-deps-push "$@"; then
@@ -319,10 +440,10 @@ function git-deps {
 
 		*)
 			# TODO: each?
-			echo '
-Usage: git deps <subcommand> [options]
+			echo "
+Usage: $GIT_DEPS_MODE-deps <subcommand> [options]
 
-git-deps is an alternative to git-submodules that keeps dependencies in
+$GIT_DEPS_MODE-deps is an alternative to submodules that keeps dependencies in
 sync.
 
 Available subcommands:
@@ -331,8 +452,10 @@ Available subcommands:
   pull [PATH]                Pulls (and update) dependencies
   push [PATH]                Push  (and update) dependencies
   sync [PATH]                Push and then pull dependencies
+  state                      Shows the current state
+  save                       Saves the current state to $GIT_DEPS_FILE
 
-'
+"
 	;;
 	esac
 }
